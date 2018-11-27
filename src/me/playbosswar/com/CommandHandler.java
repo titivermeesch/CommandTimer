446269c4742aa1a33e8c8f63a343c8440784af33
PlayBossWar <titivermeesch@gmail.com>
2018-11-27 22:24:01 +0100

    Small bug fixes

--- src/me/playbosswar/com/CommandHandler.java
+++ src/me/playbosswar/com/CommandHandler.java
@@ -1,6 +1,7 @@
 package me.playbosswar.com;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,17 +13,14 @@ public class CommandHandler implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLable, String[] args) {
 		if(sender instanceof Player) {
 			Player p = (Player) sender;
-			Bukkit.getConsoleSender().sendMessage("Player : " + p + " has All Permissions : " + p.isOp() + " and has permission even if not needed : " + p.hasPermission("commandtimer.use"));
 			if(p.hasPermission("commandtimer.use") || p.isOp()) {
 				GUIHandler.generateGUI(p);
 			} else {
-				p.sendMessage("§cYou don't have the right permission for this");
+				p.sendMessage(ChatColor.RED + "You don't have the right permission for this");
 			}
-			
 			return true;
-		} else {
-			sender.sendMessage("§Command can only be used in-game");
 		}
-		return false;
+        sender.sendMessage(ChatColor.RED + "Command can only be used in-game");
+		return true;
 	}
 }
