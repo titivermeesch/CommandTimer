c36be9ce981ce0dc55daf66bb0a3abda5d91f790
Tristan Vermeesch <titivermeesch@gmail.com>
2022-11-12 16:56:58 +0100

    fix: fix wrongly calculated day placeholder

--- src/main/java/me/playbosswar/com/utils/TaskTimeUtils.java
+++ src/main/java/me/playbosswar/com/utils/TaskTimeUtils.java
@@ -1,13 +1,16 @@
 package me.playbosswar.com.utils;
 
+import io.sentry.Sentry;
 import me.playbosswar.com.tasks.Task;
 import me.playbosswar.com.tasks.TaskTime;
+import org.bukkit.Bukkit;
 import org.jetbrains.annotations.Nullable;
 import org.joda.time.Duration;
 import org.joda.time.Interval;
 
 import java.time.DayOfWeek;
 import java.util.*;
+import java.util.logging.Level;
 import java.util.stream.Collectors;
 
 public class TaskTimeUtils {
@@ -23,17 +26,18 @@ public class TaskTimeUtils {
                     cal.set(Calendar.HOUR_OF_DAY, taskTime.getTime1().getHour());
                     cal.set(Calendar.MINUTE, taskTime.getTime1().getMinute());
                     cal.set(Calendar.SECOND, taskTime.getTime1().getSecond());
-                    cal.set(Calendar.DAY_OF_WEEK, DayOfWeek.valueOf(day).getValue());
+                    cal.set(Calendar.DAY_OF_WEEK, DayOfWeek.valueOf(day).getValue() + 1);
                     cal.set(Calendar.WEEK_OF_MONTH, finalI);
                     dates.add(cal.getTime());
                 });
             }
-
         });
 
         if(dates.size() == 0) {
             return null;
         }
+
+        System.out.println(dates);
 
         final long now = System.currentTimeMillis();
         List<Date> futureDates = dates.stream().filter(date -> date.getTime() >= now).collect(Collectors.toList());
@@ -41,6 +45,8 @@ public class TaskTimeUtils {
         if(futureDates.size() == 0) {
             return null;
         }
+
+        System.out.println(futureDates);
 
         return Collections.min(futureDates, (d1, d2) -> {
             long diff1 = Math.abs(d1.getTime() - now);
@@ -50,9 +56,17 @@ public class TaskTimeUtils {
     }
 
     public static boolean hasPassedInterval(Task task) {
-        Interval interval = new Interval(task.getLastExecuted().getTime(), new Date().getTime());
-        Duration period = interval.toDuration();
+        try {
+            Interval interval = new Interval(task.getLastExecuted().getTime(), new Date().getTime());
+            Duration period = interval.toDuration();
 
-        return period.getStandardSeconds() >= task.getInterval().toSeconds();
+            return period.getStandardSeconds() >= task.getInterval().toSeconds();
+        } catch(IllegalArgumentException e) {
+            Bukkit.getLogger().log(Level.SEVERE, "Last executed time seems to be in the future!");
+            e.printStackTrace();
+            Sentry.captureException(e);
+        }
+
+        return false;
     }
 }
