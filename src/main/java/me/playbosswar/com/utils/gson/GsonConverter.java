9c8b03e592448a66f93f8c0d77902685a529d5a6
Tristan Vermeesch <titivermeesch@gmail.com>
2024-05-31 20:24:55 +0200

    fix: use ISO time format as standard parsing format

--- src/main/java/me/playbosswar/com/utils/gson/GsonConverter.java
+++ src/main/java/me/playbosswar/com/utils/gson/GsonConverter.java
@@ -3,7 +3,6 @@ package me.playbosswar.com.utils.gson;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import java.io.Reader;
 import java.lang.reflect.Modifier;
 import java.lang.reflect.Type;
 import java.time.LocalTime;
